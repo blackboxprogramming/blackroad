3f337fdae77b821b71d85bc9282a31baebcea782
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-14 17:09:15 -0700

    Create index.ts

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,16 @@
+export * from './components/Button';
+export * from './components/Input';
+export * from './components/Tabs';
+export * from './components/Drawer';
+export * from './components/Dialog';
+export * from './components/Toast';
+export * from './components/DataTable';
+
+// Re-export individual components for convenient named imports
+export { default as Button } from './components/Button';
+export { default as Input } from './components/Input';
+export { default as Tabs } from './components/Tabs';
+export { default as Drawer } from './components/Drawer';
+export { default as Dialog } from './components/Dialog';
+export { default as Toast } from './components/Toast';
+export { default as DataTable } from './components/DataTable';
