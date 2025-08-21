b5892048f550f3d7bb886663e892cc080d2fa583
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-21 16:15:32 -0500

    Add MDX loader dependency and configure plugin

--- apps/roadbook/next.config.js
+++ apps/roadbook/next.config.js
@@ -1,6 +1,7 @@
 const withMDX = require('@next/mdx')({
-  extension: /\.(mdx?)$/.source ? undefined : /\.(mdx?)$/, // placeholder to satisfy require, not necessary
+  extension: /\.mdx?$/,
 });
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 });
